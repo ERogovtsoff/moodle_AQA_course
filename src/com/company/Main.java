577064ed7ea35fbc3d290b845426e94fbf121b22
Yahor Rahautsou <50799290+ERogovtsoff@users.noreply.github.com>
2022-07-04 15:53:57 +0300

    Added 'c' variable

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         int a = 10;
         int b = a++;
+        int c = ++a
         System.out.println("a = " + a);
         System.out.println("b = " + b);
         System.out.println(a + b);
