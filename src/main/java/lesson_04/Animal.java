f27e626232733bf191b442cf5b716b75e40da2a3
ERogovtsoff <mircourage@gmail.com>
2022-08-30 12:59:03 +0300

    Lesson_05

--- src/main/java/lesson_04/Animal.java
+++
@@ -1,6 +0,0 @@
-package lesson_04;
-
-public abstract class Animal {
-    abstract void run(int range);
-    abstract void swim(int range);
-}
--- src/main/java/lesson_04/ParkApp.java
+++
@@ -1,9 +0,0 @@
-package lesson_04;
-
-public class ParkApp {
-    public static void main(String[] args) {
-        Park.Attraction att1 = new Park().new Attraction("Колесо обозрения",
-                "10:00", "22:00", 600);
-        System.out.println(att1.toString());
-    }
-}
